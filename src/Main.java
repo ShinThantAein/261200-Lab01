955eaa12052344c528e6a8b91d01c6730cd60275
Sally <shinthantaein320@gmail.com>
2026-07-06 01:31:01 +0700

    mountain rain and grass

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,20 @@
 public class Main {
     public static void main(String[] args){
+
         Turtle drizzle = new Turtle();
+
+        // Background
+        drizzle.bgcolor("light blue");
 
         drizzle.width(3);
 
-        //Draw rain
-        drizzle.penColor("blue");
+        // RAIN
+        drizzle.penColor("white");
 
         drizzle.up();
-        drizzle.backward(280);   // move to left
+        drizzle.backward(280);
         drizzle.left(90);
-        drizzle.forward(200);    // move to top
+        drizzle.forward(200);
         drizzle.right(90);
 
         for(int row = 0; row < 4; row++){
@@ -32,6 +36,61 @@ public class Main {
             drizzle.right(90);
             drizzle.forward(35);
             drizzle.left(90);
+        }
+
+        // MOUNTAINS
+        drizzle.penColor("green");
+        drizzle.width(4);
+
+        drizzle.up();
+        drizzle.home();
+        drizzle.backward(320);
+        drizzle.right(90);
+        drizzle.forward(120);
+        drizzle.left(90);
+        drizzle.down();
+
+        for(int i = 0; i < 5; i++){
+
+            drizzle.left(60);
+            drizzle.forward(140);
+
+            drizzle.right(120);
+            drizzle.forward(140);
+
+            drizzle.left(60);
+
+        }
+
+        // GROUND
+        drizzle.up();
+        drizzle.home();
+        drizzle.backward(330);
+        drizzle.right(90);
+        drizzle.forward(210);
+        drizzle.left(90);
+        drizzle.down();
+
+        drizzle.width(3);
+        drizzle.forward(700);
+
+
+        // GRASS
+        drizzle.backward(700);
+
+        drizzle.width(2);
+
+        for(int i = 0; i < 23; i++){
+
+            drizzle.left(60);
+            drizzle.forward(20);
+
+            drizzle.right(120);
+            drizzle.forward(20);
+
+            drizzle.left(60);
+            drizzle.forward(10);
+
         }
 
     }
